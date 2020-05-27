625b02be0ec5ea97de909ca30d41d274ffefc024
redhwanzaman1989 <redhwanzaman1989@gmail.com>
2020-05-27 12:03:55 +0100

    finished chapter 1, started chapter 2

--- AutomateUsingPython/Chapter1.py
+++ AutomateUsingPython/Chapter1.py
@@ -1,1 +1,3 @@
 'Alice'+'Bob'
+'''chapter 1 is finished
+#nothing important discussed at all lol
---
+++ AutomateUsingPython/Chapter2.py
@@ -0,0 +1,6 @@
+#learn if then else controls
+#use break to end a loop
+#continue means iterate to the next loop
+#range function learned already
+#learn importing
+#like import matlibplot.pyplot etc.
