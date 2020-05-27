700812e6fe1bb8d93a6763a68552b19964315259
redhwanzaman1989 <redhwanzaman1989@gmail.com>
2020-05-27 12:16:07 +0100

    Started chapter 3 - functions on page 91 out of 505

---
+++ AutomateUsingPython/Chapter3.py
@@ -0,0 +1,21 @@
+#a function is like a mini program within a program
+def hello():
+    print('howdy')
+    print('Howdy!!!')
+    print('Hello there.')
+
+hello()
+
+def hello(name):
+    print('Hello {}'.format(name))
+
+hello('Red')
+
+#in functions you can return a value if necessary
+
+#in pythone None is the same as Null
+
+#in a function with no return statement, None is automatically declared - 
+#similar to how in a loop there is an implicit continue statement
+
+#on page 91 out of 505
