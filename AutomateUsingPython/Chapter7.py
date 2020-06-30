993149ec4a7493400a6ddb2775f8922ab14d2ded
redhwanzaman1989 <redhwanzaman1989@gmail.com>
2020-06-30 11:34:04 +0100

    still working chapter 7 - learning searches etc.

--- AutomateUsingPython/Chapter7.py
+++ AutomateUsingPython/Chapter7.py
@@ -20,3 +20,20 @@ print('415-555-4242 is a phone number:')
 print(isPhoneNumber('415-555-4242'))
 print('Moshi moshi is a phone number:')
 print(isPhoneNumber('Moshi moshi'))
+
+#this is smart - for every letter in a string, they look at the next 12
+#letters to form a string, then they look to see whether this string is a 
+#phone number.
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: ' + chunk)
+        print('Done')
+
+#regular expressions are called regexes for short
+#examples of regexes are \d which refers to a digit character
+#\n as a new line \p as a new paragraph \t or 09x as a tab etc
+#
+print('red\n')
+print(r'red\n')
