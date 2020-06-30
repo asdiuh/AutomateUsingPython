665e6cb0384e2c18d1aae6315e17bd41217e86a3
redhwanzaman1989 <redhwanzaman1989@gmail.com>
2020-06-30 11:10:43 +0100

    started chapter 7

---
+++ AutomateUsingPython/Chapter6.py
@@ -0,0 +1,8 @@
+print('a backslash will allow you to place a single quote (\') into text')
+#r before the print means raw string - removes escape characters
+print(r'a backslash will allow you to place a single quote (\') into text')
+print(''' you can print 
+      multiline things with triple quotes (\''') ''')
+
+spam = 'Hello World!'
+fizz = spam[0:7]
