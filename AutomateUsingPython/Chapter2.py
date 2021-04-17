7875250dc65ebe2898b442c4ecd25b67474d16c4
Redhwan Zaman <57918009+asdiuh@users.noreply.github.com>
2021-04-17 01:09:28 +0100

    Tested basic if statement
    tested basic if statement and made a basic function called Collatz - the Collatz sequence

--- AutomateUsingPython/Chapter2.py
+++ AutomateUsingPython/Chapter2.py
@@ -6,3 +6,12 @@
 #like import matlibplot.pyplot etc.
 #import and from are the same
 #finished chapter 2
+
+def collatz(InputNumber):
+    if InputNumber%2 == 0:
+        print("InputNumber is even and output is: ",InputNumber//2)
+    elif InputNumber%2 == 1:
+        print("InputNumber is odd and output is: ",3*InputNumber+1)
+            
+collatz(4)        
+            
